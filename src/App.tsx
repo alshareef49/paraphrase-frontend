21ff2e73e8495397ea269cccc7fa6f4493fe6e1b
Shareef Alam <alshareef4039@gmail.com>
2025-05-14 17:25:30 +0530

    code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,6 @@ import React from "react";
 import TextEditor from "./components/TextEditor";
 
 const App: React.FC = () => {
-  const handleParaphrase = async (
-    selectedText: string,
-    updateText: (newText: string) => void
-  ) => {
-    try {
-      // Send the selected text to the backend for paraphrasing
-      const response = await fetch("http://localhost:8000/paraphrase", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text: selectedText }), // Passing selected text
-      });
-
-      // Parse the response to get the paraphrased text
-      const data = await response.json();
-
-      // Update the editor with the paraphrased text
-      updateText(data.paraphrased);
-    } catch (error) {
-      console.error("Failed to paraphrase:", error);
-    }
-  };
 
   return (
     <div className="min-h-screen bg-gray-50 p-4">
